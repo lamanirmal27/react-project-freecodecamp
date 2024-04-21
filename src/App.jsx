bba9d5c6e52b3d8c1c4fbcb83760ae5bd9be464f
Nirmal Tamang <lamanirmal27@gmail.com>
2024-04-21 21:00:06 +0545

    Star Rating Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 export default function App() {
   return (<>
-    <RandomColor/>
+    {/* Random Color Genertor component */}
+   {/* <RandomColor/> */ }
+
+   {/* Star Rating Component */}
+   <StarRating noOfStars={10}/>
   </>)
 }
