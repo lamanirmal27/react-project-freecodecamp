45e9732d5fee730241ebd080816baab08aa49fe0
Nirmal Tamang <lamanirmal27@gmail.com>
2024-04-22 14:15:40 +0545

    image slide component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import RandomColor from "./components/random-color";
-import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+//import RandomColor from "./components/random-color";
+//import StarRating from "./components/star-rating";
 
 export default function App() {
   return (<>
@@ -7,6 +8,9 @@ export default function App() {
    {/* <RandomColor/> */ }
 
    {/* Star Rating Component */}
-   <StarRating noOfStars={10}/>
+   {/* <StarRating noOfStars={10}/> */}
+
+   {/* Image Slider Component */}
+   <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} page={"1"}/>
   </>)
 }
